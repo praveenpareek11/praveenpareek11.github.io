5b13659264336dd04f7df2f06087aa7d6a89ce70
Praveen Pareek <praveenpareek@Praveens-MacBook-Pro.local>
2022-12-05 14:07:28 +0530

    fixed the bug

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Hotel Review Analysis",
+        "excerpt":"NLP and Time Series Analysis Project The purpose is: To analyse the Negative Review (Text format) using NLP techniques to extract the keywords which are causing the negative reviews. It outputs the top 100 keywords each in unigram, bigram and trigram category. e.g. unigram: service, poor, old, noisy, dirty, rude,...","categories": ["Blog"],
+        "tags": ["machine learning","python","data science","natural language processing","time series analysis"],
+        "url": "/blog/reviewanalysis/",
+        "teaser": null
+      },{
+        "title": "Anamoly Detection",
+        "excerpt":"Credit Card Fraud Detection Introduction In this kernel we will use various predictive models to see how accurate they are in detecting whether a transaction is a normal payment or a fraud. As described in the dataset, the features are scaled and the names of the features are not shown...","categories": ["Blog"],
+        "tags": ["machine learning","python","data science"],
+        "url": "/blog/anamoly_detection/",
+        "teaser": null
+      },{
+        "title": "RNA Seq Analysis",
+        "excerpt":"RNA Seq Analysis Introduction Pancreatic Adenocarcinoma (PAAD) is the third most common cause of death from cancer, with an overall 5-year survival rate of less than 5%, and is predicted to become the second leading cause of cancer mortality in the United States by 2030. Ribonucleic acid (​ RNA​ )...","categories": ["Blog"],
+        "tags": ["rna seq","principal component analysis","python","data science","rna sequencing","visualization"],
+        "url": "/blog/RNA-Seq/",
+        "teaser": null
+      },{
+        "title": "Purchase Behaviour of Users in Online Retail",
+        "excerpt":"Introduction: This data is about the users who actually purchased some items after browsing the website and visited certain parts of website Problem Statement: Customer segmentation is the problem of uncovering information about a firm’s customer base, based on their interactions with the business. In most cases this interaction is...","categories": ["Blog"],
+        "tags": ["customer segmentation","market analysis","python","data science","k-means clustering","visualization","online retail"],
+        "url": "/blog/Digital-Marketing/",
+        "teaser": null
+      }]
